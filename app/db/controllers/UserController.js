37125831378d6f946c444420637863eb3eab06e7
SarvarbekMuminjonov <smomimjonov20@gmail.com>
2022-04-08 10:20:48 +0500

    controllers changed

--- app/db/controllers/UserController.js
+++ app/db/controllers/UserController.js
@@ -1,11 +1,26 @@
 import User from "./../models/model.js"
+import sequlize from '../db.js'
+const GET_USER =  async (req, res) => {
+  try {
+    const data = await sequlize.query('select first_name,last_name,picture from users')
+    res.json(data)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
 
-const getUser =()=> User.findAll()
-
-const saveUser = (first_name, last_name, picture) =>
-  User.create({
-    first_name,
-    last_name,
-    picture,
-  })
-export { getUser, saveUser }
+const CREATE_USER = async (req, res) => {
+  try {
+    const body = req.body
+    let { first_name, last_name, picture } = body
+    await User.create({
+      first_name,
+      last_name,
+      picture,
+    })
+    res.send("ok")
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+export { GET_USER,CREATE_USER }
