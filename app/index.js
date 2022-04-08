75b6b0d01e5e04cb48bae7fb2c56c954def03db1
SarvarbekMuminjonov <smomimjonov20@gmail.com>
2022-04-08 10:47:21 +0500

    controllers changed

--- app/index.js
+++ app/index.js
@@ -3,9 +3,10 @@ const app = express()
 import bodypareser from "body-parser"
 import router from "./routes/index.js"
 import { port } from "./config/index.js"
+const PORT = process.env.PORT || port
 
-
+app.use(express.json())
 app.use(bodypareser.json())
 app.use("/", router)
 
-app.listen(process.env.PORT || port, () => console.log(`Application started on Port ${port}.`))
+app.listen(PORT, () => console.log(`Application started on Port ${PORT}.`))
